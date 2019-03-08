570be1e762f8a2d7cae20d64b8e3b47e21f7c2a2
Shubham Padia <shubhamapadia@gmail.com>
2019-03-08 16:48:10 +0800

    Use react-jsonschema-form-semanticui

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,77 @@
 import React, { Component } from 'react'
-import { Form } from 'semantic-ui-react'
+import Form from 'react-jsonschema-form-semanticui'
+
+const schema = {
+  "title": "",
+  "type": "object",
+  "required": [
+    "version"
+  ],
+  "properties": {
+    "version": {
+      "type": "string",
+      "title": "Version"
+    },
+    "metadata": {
+      "type": "string",
+      "format": "data-url",
+      "title": "Metadata File"
+    },
+    "session_steps": {
+      "type": "array",
+      "title": "Session Steps",
+      "items": {
+        "type": "object",
+        "required": [
+          "session_num"
+        ],
+        "properties": {
+          "session_num": {
+            "type": "string",
+            "title": "Session Number"
+          },
+          "steps": {
+            "type": "array",
+            "title": "Steps",
+            "items": {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "number",
+                  "title": "Step ID"
+                },
+                "task_type": {
+                  "type": "string",
+                  "title": "Task"
+                },
+                "parents": {
+                  "type": "string",
+                  "title": "Parents"
+                },
+                "inputs": {
+                  "type": "string",
+                  "title": "Inputs"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+const log = (type) => console.log.bind(console, type);
 
 export class Home extends Component {
   render() {
     return (
       <div>
         <h1 style={{ textAlign: "center", marginBottom: "30px" }}> Create Pipeline </h1>
-        <Form>
-          <Form.Group widths='equal'>
-            <Form.Input
-              fluid
-              id='form-subcomponent-shorthand-input-version'
-              label='Version'
-              placeholder='Version'
-            />
-            <Form.Input
-              fluid
-              type='date'
-              id='form-subcomponent-shorthand-input-recording-date'
-              label='Recording Date'
-              placeholder='Recording Date'
-            />
-          </Form.Group>
-        </Form>
+        <Form schema={schema}
+          onChange={log("changed")}
+          onSubmit={log("submitted")}
+          onError={log("errors")} />
       </div>
     )
   }
