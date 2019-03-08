5694db189dfa39704a16c51a7ab3e8fd1998a233
Shubham Padia <shubhamapadia@gmail.com>
2019-03-08 17:12:12 +0800

    temp

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,60 +2,70 @@ import React, { Component } from 'react'
 import Form from 'react-jsonschema-form-semanticui'
 
 const schema = {
-  "title": "",
-  "type": "object",
-  "required": [
+  title: "",
+  type: "object",
+  required: [
     "version"
   ],
-  "properties": {
-    "version": {
-      "type": "string",
-      "title": "Version"
+  properties: {
+    version: {
+      type: "string",
+      title: "Version"
     },
-    "metadata": {
-      "type": "string",
-      "format": "data-url",
-      "title": "Metadata File"
+    metadata: {
+      type: "string",
+      format: "data-url",
+      title: "Metadata File"
     },
-    "session_steps": {
-      "type": "array",
-      "title": "Session Steps",
-      "items": {
-        "type": "object",
-        "required": [
+    session_steps: {
+      type: "array",
+      title: "Session Steps",
+      items: {
+        type: "object",
+        required: [
           "session_num"
         ],
-        "properties": {
-          "session_num": {
-            "type": "string",
-            "title": "Session Number"
+        properties: {
+          session_num: {
+            type: "string",
+            title: "Session Number"
           },
-          "steps": {
-            "type": "array",
-            "title": "Steps",
-            "items": {
-              "type": "object",
-              "properties": {
-                "id": {
-                  "type": "number",
-                  "title": "Step ID"
+          steps: {
+            type: "array",
+            title: "Steps",
+            items: {
+              type: "object",
+              properties: {
+                id: {
+                  type: "number",
+                  title: "Step ID"
                 },
-                "task_type": {
-                  "type": "string",
-                  "title": "Task"
+                task_type: {
+                  type: "string",
+                  title: "Task"
                 },
-                "parents": {
-                  "type": "string",
-                  "title": "Parents"
+                parents: {
+                  type: "string",
+                  title: "Parents"
                 },
-                "inputs": {
-                  "type": "string",
-                  "title": "Inputs"
+                inputs: {
+                  type: "string",
+                  title: "Inputs"
                 }
               }
             }
           }
         }
+      }
+    }
+  }
+};
+
+const uiSchema = {
+  session_steps: {
+    steps: {
+      inputs: {
+        "ui:widget": "textarea"
       }
     }
   }
@@ -69,6 +79,7 @@ export class Home extends Component {
       <div>
         <h1 style={{ textAlign: "center", marginBottom: "30px" }}> Create Pipeline </h1>
         <Form schema={schema}
+          uiSchema={uiSchema}
           onChange={log("changed")}
           onSubmit={log("submitted")}
           onError={log("errors")} />
