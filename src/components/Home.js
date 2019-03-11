56dc39ed0fc3ee30f2e01a46c8c1794fc3b51d2c
Shubham Padia <shubhamapadia@gmail.com>
2019-03-11 15:43:21 +0800

    Make metadata a text area and the inputs a textarea

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,55 +2,54 @@ import React, { Component } from 'react'
 import Form from 'react-jsonschema-form-semanticui'
 
 const schema = {
-  title: "",
-  type: "object",
-  required: [
+  "title": "",
+  "type": "object",
+  "required": [
     "version"
   ],
-  properties: {
-    version: {
-      type: "string",
-      title: "Version"
+  "properties": {
+    "version": {
+      "type": "string",
+      "title": "Version"
     },
-    metadata: {
-      type: "string",
-      format: "data-url",
-      title: "Metadata File"
+    "metadata": {
+      "type": "string",
+      "format": "textarea",
+      "title": "Metadata"
     },
-    session_steps: {
-      type: "array",
-      title: "Session Steps",
-      items: {
-        type: "object",
-        required: [
-          "session_num"
+    "session_steps": {
+      "type": "array",
+      "title": "Session Steps",
+      "items": {
+        "type": "object",
+        "required": [
         ],
-        properties: {
-          session_num: {
-            type: "string",
-            title: "Session Number"
+        "properties": {
+          "session_num": {
+            "type": "string",
+            "title": "Session Number"
           },
-          steps: {
-            type: "array",
-            title: "Steps",
-            items: {
-              type: "object",
-              properties: {
-                id: {
-                  type: "number",
-                  title: "Step ID"
+          "steps": {
+            "type": "array",
+            "title": "Steps",
+            "items": {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "number",
+                  "title": "Step ID"
                 },
-                task_type: {
-                  type: "string",
-                  title: "Task"
+                "task_type": {
+                  "type": "string",
+                  "title": "Task"
                 },
-                parents: {
-                  type: "string",
-                  title: "Parents"
+                "parents": {
+                  "type": "string",
+                  "title": "Parents"
                 },
-                inputs: {
-                  type: "string",
-                  title: "Inputs"
+                "inputs": {
+                  "type": "string",
+                  "title": "Inputs"
                 }
               }
             }
@@ -62,10 +61,17 @@ const schema = {
 };
 
 const uiSchema = {
-  session_steps: {
-    steps: {
-      inputs: {
-        "ui:widget": "textarea"
+  "session_steps": {
+    "items": {
+      "steps": {
+        "items": {
+          "inputs": {
+            "ui:widget": "textarea",
+            "ui:options": {
+              "rows": 5
+            }
+          }
+        }
       }
     }
   }
