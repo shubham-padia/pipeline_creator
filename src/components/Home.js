30836c5730599ce6fc813c9c86e8b29e3d4dc72e
Shubham Padia <shubhamapadia@gmail.com>
2019-03-15 11:54:40 +0800

    Add help modal with input formats for each task.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Form from 'react-jsonschema-form'
 import './home.css'
 import 'bootstrap/dist/css/bootstrap.css';
-import { throws } from 'assert';
+import Popup from "reactjs-popup";
 
 const schema = {
   "title": "",
@@ -172,7 +172,41 @@ export class Home extends Component {
   render() {
     return (
       <div>
-        <h1 style={{ textAlign: "center", marginBottom: "30px" }}> Create Pipeline </h1>
+        <div style={{marginBottom: "30px"}}>
+        <h1 style={{ textAlign: "center", margin: "auto", display: "inline" }} > Create Pipeline </h1>
+        <Popup
+          trigger={<button className="help-button btn btn-info"> Help </button>}
+          modal
+          closeOnDocumentClick>
+          <div style={{whiteSpace: "pre"}}>
+            <h3> {"Input Formats:"} </h3>
+            <h5>{"dummy:"}</h5>
+            {'{}'}
+            <h5>{"resample:"}</h5>
+            {'{"mic_name": ""}'}
+            <h5>{"vad:"}</h5>
+            {'{"mic_name": ""}'}
+            <h5>{"diarization:"}</h5>
+            {'{\
+              \n    "mic_name": "table",\
+              \n    "speaker_id": 1\
+              \n}'}
+            <h5>{"decoder:"}</h5>
+            {'{\
+            \n    "seg": { \
+            \n        "mic_name": "table", \
+            \n        "source": "diarization", \
+            \n        "speaker_id": 1 \
+            \n    }, \
+            \n    "wav": { \
+            \n        "mic_name": "ceiling", \
+            \n        "source": "vad", \
+            \n        "speaker_id": 1 \
+            \n    } \
+            \n}'}
+          </div>
+        </Popup>
+        </div>
         <Form schema={schema}
           uiSchema={uiSchema}
           onChange={log("changed")}
