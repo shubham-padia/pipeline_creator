b4b9c6ee91bb39b6391db509894d8bc4983ab029
Shubham Padia <shubhamapadia@gmail.com>
2019-03-18 11:29:40 +0800

    Move schema definitions to SchemaDefinitions.js

---
+++ src/components/SchemaDefinitions.js
@@ -0,0 +1,117 @@
+export const schema = {
+    "title": "",
+    "type": "object",
+    "required": [
+        "name",
+        "version",
+        "metadata",
+        "steps"
+    ],
+    "properties": {
+        "name": {
+            "type": "string",
+            "title": "Name"
+        },
+        "version": {
+            "type": "string",
+            "title": "Version",
+            "enum": ["0.0.1"],
+            "default": "0.0.1"
+        },
+        "metadata": {
+            "type": "string",
+            "title": "Metadata"
+        },
+        "steps": {
+            "type": "array",
+            "title": "Steps",
+            "minItems": 1,
+            "items": {
+                "type": "object",
+                "required": [
+                    "session_num",
+                    "steps_for_session"
+                ],
+                "properties": {
+                    "session_num": {
+                        "type": "number",
+                        "title": "Session Number",
+                    },
+                    "steps_for_session": {
+                        "type": "array",
+                        "title": "Steps for this session",
+                        "minItems": 1,
+                        "items": {
+                            "type": "object",
+                            "required": [
+                                "id",
+                                'task_type',
+                                'inputs'
+                            ],
+                            "properties": {
+                                "id": {
+                                    "type": "number",
+                                    "title": "Step ID"
+                                },
+                                "task_type": {
+                                    "type": "string",
+                                    "title": "Task",
+                                    "enum": ["dummy", "resample", "vad", "diarization", "decoder"],
+                                    "default": "dummy"
+                                },
+                                "parent_id": {
+                                    "type": "string",
+                                    "title": "Parents"
+                                },
+                                "inputs": {
+                                    "type": "string",
+                                    "title": "Inputs",
+                                    "default": "{}"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+const twoWide = {
+    classNames: "col-md-4"
+}
+
+export const uiSchema = {
+    "metadata": {
+        "ui:widget": "textarea",
+        "ui:options": {
+            "rows": 8
+        }
+    },
+    "steps": {
+        "ui:options": {
+            "orderable": false
+        },
+        "items": {
+            classNames: "object",
+            "steps_for_session": {
+                "ui:options": {
+                    "orderable": false
+                },
+                "items": {
+                    classNames: "object",
+                    "id": twoWide,
+                    "task_type": twoWide,
+                    "parent_id": twoWide,
+                    "inputs": {
+                        "ui:widget": "textarea",
+                        "ui:options": {
+                            "rows": 5
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
