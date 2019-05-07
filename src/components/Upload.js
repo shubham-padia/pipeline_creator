a7f598dcf537cc854a59dcd304ed78e16a10d209
Shubham Padia <shubhamapadia@gmail.com>
2019-05-07 12:11:52 +0800

    Initial working upload

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import DropzoneComponent from 'react-dropzone-component';
+import { Button, Form } from 'semantic-ui-react';
+import { cloneDeep } from 'lodash';
 
 export class Upload extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            fileList: []
+        }
 
         // For a full list of possible configurations,
         // please consult http://www.dropzonejs.com/#configuration
@@ -15,7 +20,7 @@ export class Upload extends Component {
         this.componentConfig = {
             iconFiletypes: ['.jpg', '.png', '.gif'],
             showFiletypeIcon: true,
-            postUrl: '/uploadHandler'
+            postUrl: 'no-url'
         };
 
         // If you want to attach multiple callbacks, simply
@@ -25,11 +30,33 @@ export class Upload extends Component {
         // Simple callbacks work too, of course
         this.callback = () => console.log('Hello!');
 
-        this.success = file => console.log('uploaded', file);
+        this.addedfile = file => {
+            let fileList = cloneDeep(this.state.fileList)
+            fileList.push(file);
+            this.setState({
+                fileList
+            });
+        }
 
         this.removedfile = file => console.log('removing...', file);
 
         this.dropzone = null;
+    }
+
+    handleSubmit = (event) => {
+        // Prevent default behavior
+        event.preventDefault();
+
+        var data = new FormData(event.target);
+
+        for (let f of this.state.fileList) {
+            data.append('file', f);
+        }
+
+        fetch('http://localhost:8000/api/v1/upload-audio', {
+            method: 'POST',
+            body: data
+        })
     }
 
     render() {
@@ -40,12 +67,24 @@ export class Upload extends Component {
         const eventHandlers = {
             init: dz => this.dropzone = dz,
             drop: this.callbackArray,
-            addedfile: this.callback,
-            success: this.success,
+            addedfile: this.addedfile,
             removedfile: this.removedfile
         }
 
-        return <DropzoneComponent config={config} eventHandlers={eventHandlers} djsConfig={djsConfig} />
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Field>
+                    <label>Recording ID</label>
+                    <input name="recording_id" />
+                </Form.Field>
+                <Form.Field>
+                    <label>Pipeline ID</label>
+                    <input name="pipeline_id" />
+                </Form.Field>
+                <DropzoneComponent config={config} eventHandlers={eventHandlers} djsConfig={djsConfig} />
+                <Button type='submit' >Submit</Button>
+            </Form>
+        )
     }
 }
 
